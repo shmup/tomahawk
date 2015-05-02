bc2510b1c1bb24e56617dcef3d245d899a732303
Jared Miller <jtmiller@gmail.com>
2015-05-02 15:21:59 -0400

    started prototyping the jquery-ui dialog for a new message. monitors character count via knockout/jquery

---
+++ Tomahawk/Scripts/tomahawk.js
@@ -0,0 +1,32 @@
+﻿function init() {
+    ko.applyBindings(vm, document.getElementById("wrapper"))
+}
+
+$(document).ready(init)
+
+var vm = function () {
+    var self = this
+
+    self.isOpen = ko.observable(false)
+
+    self.message = ko.observable("")
+
+    self.canSend = ko.observable(true)
+
+    self.getCharacterCount = ko.computed(function () {
+        var limit = 10,
+            count = self.message().length
+
+        if (count > limit) {
+            self.canSend(false)
+        } else {
+            self.canSend(true)
+        }
+
+        return (limit - count)
+    })
+
+    self.openMessage = function () {
+        self.isOpen(true)
+    }
+}
