7b5a24be0162e06e222bc87352c5938e83c81dbc
Jared Miller <jtmiller@gmail.com>
2015-05-02 20:33:53 -0400

    knockout loading messages

--- Tomahawk/Scripts/tomahawk.js
+++ Tomahawk/Scripts/tomahawk.js
@@ -1,32 +1,41 @@
 ﻿function init() {
-    ko.applyBindings(vm, document.getElementById("wrapper"))
 }
 
-$(document).ready(init)
+$(document).ready(function () {
+    var vm = function () {
+        var self = this
 
-var vm = function () {
-    var self = this
+        self.messages = ko.observableArray([])
 
-    self.isOpen = ko.observable(false)
+        self.isOpen = ko.observable(false)
 
-    self.message = ko.observable("")
+        self.message = ko.observable("")
 
-    self.canSend = ko.observable(true)
+        self.canSend = ko.observable(true)
 
-    self.getCharacterCount = ko.computed(function () {
-        var limit = 10,
-            count = self.message().length
+        self.getCharacterCount = ko.computed(function () {
+            var limit = 10,
+                count = self.message().length
 
-        if (count > limit) {
-            self.canSend(false)
-        } else {
-            self.canSend(true)
+            if (count > limit) {
+                self.canSend(false)
+            } else {
+                self.canSend(true)
+            }
+
+            return (limit - count)
+        })
+
+        self.openMessage = function () {
+            self.isOpen(true)
         }
-
-        return (limit - count)
-    })
-
-    self.openMessage = function () {
-        self.isOpen(true)
     }
-}
+
+    var viewModel = new vm();
+
+    ko.applyBindings(viewModel, document.getElementById("wrapper"))
+
+    $.getJSON("/Messages/All", function (data) {
+        viewModel.messages(data)
+    })
+})
